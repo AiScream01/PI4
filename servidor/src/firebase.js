453528748b30efdcf83eabb46a52030a0ce000d5
AiScream01 <ruicosta.der@gmail.com>
2024-09-04 23:22:31 +0100

    Update firebase.js

--- servidor/src/firebase.js
+++ servidor/src/firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./caminho/para/o/teu/ficheiro-firebase-adminsdk.json');
+const serviceAccount = require('./projeto4-ff67b-firebase-adminsdk-hrler-f6439cf2ec.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
