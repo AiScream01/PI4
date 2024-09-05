3150f204a41303b9891e4c5ed60147d3c1bba8fa
Ana Figueiredo <beatriz1002figueiredo@gmail.com>
2024-09-05 03:12:37 +0100

    Update App.js

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import SideBar from './view/components/SideBar';
+import SideBar from "./view/components/SideBar";
 
 // Importar componentes para as diferentes páginas
 import Login from './view/auth/login';
-import PedidoFerias from './view/backoffice/PedidoFerias';
-import Horas from './view/backoffice/Horas';
-import Ajudas from './view/backoffice/AjudasCusto';
-import ViaturaPropria from './view/backoffice/ViaturaPropria';
-import Faltas from './view/backoffice/Faltas';
-import DadosPessoais from './view/backoffice/DadosPessoais';
-import Noticias from './view/backoffice/Noticias';
-import Parcerias from './view/backoffice/Parcerias';
-import NotificacaoPush from './view/backoffice/NotificacaoPush';
-import Reunioes from './view/backoffice/Reunioes';
-import Utilizadores from './view/backoffice/Utilizadores';
-import MicroSite from './view/backoffice/MicroSite';
+import PedidoFerias from "./view/backoffice/PedidoFerias";
+import Horas from "./view/backoffice/Horas";
+import Ajudas from "./view/backoffice/AjudasCusto"
+import ViaturaPropria from "./view/backoffice/ViaturaPropria";
+import Faltas from "./view/backoffice/Faltas";
+import DadosPessoais from "./view/backoffice/DadosPessoais";
+import Noticias from "./view/backoffice/Noticias";
+import Parcerias from "./view/backoffice/Parcerias";
+import NotificacaoPush from "./view/backoffice/NotificacaoPush";
+import Reunioes from "./view/backoffice/Reunioes";
+import Utilizadores from "./view/backoffice/Utilizadores";
+import MicroSite from "./view/backoffice/MicroSite";
 import EditarMicroSite from './view/backoffice/EditarMicroSite';
+
 
 export default function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                {/* Rota padrão sem side bar*/}
+                <Route path="/" element={(<Login />)} />
+
                 <Route path="/pedido-ferias" element={addSidebar(<PedidoFerias />)} />
                 <Route path="/horas" element={addSidebar(<Horas />)} />
                 <Route path="/ajudas" element={addSidebar(<Ajudas />)} />
@@ -38,12 +41,12 @@ export default function App() {
                 <Route path="/reunioes" element={addSidebar(<Reunioes />)} />
                 <Route path="/utilizadores" element={addSidebar(<Utilizadores />)} />
                 <Route path="/micro-site-editar" element={addSidebar(<EditarMicroSite />)} />
-                <Route path="/micro-site" element={addSidebar(<MicroSite />)} />
+                <Route path="/micro-site" element={<MicroSite />} />
             </Routes>
         </Router>
     );
 
-    function addSidebar(prop) {
+    function addSidebar(prop){
         return (
             <div className="d-flex">
                 <SideBar />
@@ -52,5 +55,7 @@ export default function App() {
                 </div>
             </div>
         );
+
     }
+
 }
