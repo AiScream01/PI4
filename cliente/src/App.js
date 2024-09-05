2b662b3ce6b0934cc6aec3481aad3b9081399162
Ana Figueiredo <beatriz1002figueiredo@gmail.com>
2024-09-05 03:59:07 +0100

    Update App.js

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -42,7 +42,7 @@ export default function App() {
                 <Route path="/utilizadores" element={addSidebar(<Utilizadores />)} />
                 <Route path="/micro-site-editar" element={addSidebar(<EditarMicroSite />)} />
                 <Route path="/micro-site" element={<MicroSite />} />
-                <Route element={addSidebar(<PedidoFerias/>)}/>
+                <Route element={addSidebar(<Login/>)}/>
             </Routes>
         </Router>
     );
