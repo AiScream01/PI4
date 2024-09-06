eada8abed6ecba9db3ee343ea7105d424128d0e9
AiScream01 <ruicosta.der@gmail.com>
2024-09-06 16:36:20 +0100

    multer em horas

--- servidor/src/routes/horasRoutes.js
+++ servidor/src/routes/horasRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const horasController = require('../controllers/horasController');
 
 // Configuração do multer para PDFs
