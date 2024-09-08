0f1e1f994460a001f0dc096c587fbfac25bdbf2a
AiScream01 <ruicosta.der@gmail.com>
2024-09-08 07:02:41 +0100

    Update config.js

--- cliente/src/config.js
+++ cliente/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001/';
-//const API_BASE_URL = 'https://pi4-api.onrender.com/';
+//const API_BASE_URL = 'http://localhost:3001/';
+const API_BASE_URL = 'https://pi4-api.onrender.com/';
 
 export default API_BASE_URL;
